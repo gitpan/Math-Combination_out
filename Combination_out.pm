51b82a6450e6a30495d87a0206e9987db7bc4262
Petar Kaleychev <kaleychev@cpan.org>
2012-02-19 17:57:18 +0000

    Import of KALEYCHEV/Math-Combination_out-0.08 from CPAN.
    gitpan-cpan-distribution: Math-Combination_out
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         KALEYCHEV/Math-Combination_out-0.08.tar.gz
    gitpan-cpan-author:       KALEYCHEV
    gitpan-cpan-maturity:     released

--- Combination_out.pm
+++ Combination_out.pm
@@ -6,104 +6,105 @@ use warnings;
 
 use Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT = qw(combinations);
-our $VERSION = '0.07';
+our @EXPORT = qw(combinations_without_repetition
+                 combinations_with_repetition);
+our $VERSION = '0.08';
 
+sub combinations_without_repetition;
+sub combinations_with_repetition;
 sub add_arr_comb;
-sub combinations;
 sub requirements;
 
-my ($count, $i, $k, $r, @words);
-sub combinations {
-	@words = @_;
-	$count = 0;
-	my $opt = pop(@words);
-	$k = pop(@words);
-	my $len_words = $#words;
-	my $n = $len_words + 1;
-	my $ret_all = "";
-	
-	if (($k <= 0) or ($n == 0) or ($k > $n)) {
-		requirements;
-	}
-	if (($opt!= 1) and ($opt!= 2)) {
-		requirements;
-	}
+sub combinations_without_repetition {
+    my $count = 0;
+    my ($ref_words, $k) = @_;
+    my @words = @$ref_words;
+    my $n = $#words + 1;
+    my $ret_all = ();
+    if ($k <= 0 or $n == 0 or $k > $n) {
+        requirements;
+    }
+    my $i;
+    my @out_num;
+    for($i = 0; $i < $k; $i++) {
+        $out_num[$i] = $i;
+    }
+    $count++;
+    $ret_all .= add_arr_comb($count, $k, \@out_num, \@words);
+    while() {
+        while($out_num[$k - 1] < $n - 1) {
+            $out_num[$k - 1]++;
+            $count++;
+            $ret_all .= add_arr_comb($count, $k, \@out_num, \@words);
+        }
+        $i = $k - 1;
+        while($i >= 0 && $out_num[$i] == $n - $k + $i) {
+            $i--;
+        }
+        if($i < 0) {
+            return $ret_all;
+        }
+        $out_num[$i]++;
+        for($i += 1; $i < $k; $i++) {
+            $out_num[$i] = $out_num[$i - 1] + 1;
+        }
+        $count++;
+        $ret_all .= add_arr_comb($count, $k, \@out_num, \@words);
+    }
+}
 
-	if($opt == 1) {
- 		for($i = 0; $i < $k; $i++) {
-			$r->[$i] = $i;
-		}
-	}
-	if($opt == 2) {
- 		for($i = 0; $i < $k; $i++) {
-			$r->[$i] = 0;
-		}
-	}
-	$count++;
+sub combinations_with_repetition {
+    my $count = 0;
+    my ($ref_words, $k) = @_;
+    my @words = @$ref_words;
+    my $n = $#words + 1;
+    my $ret_all = ();
+    if ($k <= 0 or $n == 0 or $k > $n) {
+        requirements;
+    }
+    my $i;
+    my @out_num;
+    for($i = 0; $i < $k; $i++) {
+        $out_num[$i] = 0;
+    }
+    $count++;
+    $ret_all .= add_arr_comb($count, $k, \@out_num, \@words);
+    while() {
+        $i = $k - 1;
+        while($i >= 0 && $out_num[$i] == $n - 1) {
+            $i--;
+        }
+        if($i < 0) {
+            return $ret_all;
+        }
+        $out_num[$i]++;
+        for($i += 1; $i < $k; $i++) {
+            $out_num[$i] = $out_num[$i - 1];
+        }
+        $count++;
+        $ret_all .= add_arr_comb($count, $k, \@out_num, \@words);
+    }
+}
 
-		sub add_arr_comb {
- 			my $ret = "";
- 			$ret .= "($count) ";
-			for($i = 0; $i < $k; $i++) {
-				$ret .= "$words[$r->[$i]] ";
-			}
-			$ret .= "\n";
-			return $ret;
-		}
-	$ret_all .= add_arr_comb;
-
-	while() {
-		if($opt == 1) {
-			while($r->[$k - 1] < $n - 1) {
-				$r->[$k - 1]++;
-				$count++;
-				$ret_all .= add_arr_comb;
-			}
-		}
-	$i = $k - 1;
-	
-	if($opt == 1) {
- 		while($i >= 0 && $r->[$i] == $n - $k + $i) {
-			$i--;
-		}
-	}
-	if($opt == 2) { 
-		while($i >= 0 && $r->[$i] == $n - 1) {
-			$i--;
-		}
-	}
-
-	if($i < 0) {
-		return $ret_all;
-	}
-	$r->[$i]++;
-
-	if($opt == 1) {  
-		for($i += 1; $i < $k; $i++) {
-			$r->[$i] = $r->[$i - 1] + 1;
-		}
-		$count++;
-	}
-	if($opt == 2) {
-		for($i += 1; $i < $k; $i++) {
-			$r->[$i] = $r->[$i - 1];
-		}
-		$count++;
-	}
-	$ret_all .= add_arr_comb;
-	
-	}
+sub add_arr_comb {
+    my ($count, $k, $ref_out_num, $ref_words) = @_;
+    my $ret = ();
+    $ret .= "($count) ";
+    my $i;
+    for($i = 0; $i < $k; $i++) {
+        $ret .= "$ref_words->[$ref_out_num->[$i]] ";
+    }
+    $ret .= "\n";
+    #print $ret; # view of continued progress
+    return $ret;
 }
 
 sub requirements {
-	print "Requirements:\n";
-	print "k - integer\n";
-	print "k > 0\n";
-	print "k < or = size of array\n";
-	print "opt = 1 or opt = 2\n";
-	print "Quit\n";
-	exit(-1);
+    print "Requirements:\n";
+    print "k - integer, k > 0\n";
+    print "k < or = size of array\n";
+    print "Quit\n";
+    exit(-1);
 }
 
 1;
@@ -112,70 +113,84 @@ __END__
 
 =head1 NAME
 
-Math::Combination_out - Combinations without or with repetition
-
-In this module was applied the approach for k-combinations without and with repetition in lexicographic order, 
-presented in the ANSI-C code by Siegfried Koepf at:
-
-http://www.aconnect.de/friends/editions/computer/combinatoricode_e.html
+Math::Combination_out - Combinations without/with repetition
 
 =head1 SYNOPSIS
 
-use Math::Combination_out;
+    use Math::Combination_out;
 
-{my @words = qw/a1 b2 c3 d4 e5 f6/; #array for combinatorics
-my $k = 4; #length for combinatorics, use only integer!
-my $opt = 1; #option: 1 - without repetition
+    {
+    # array for combinatorics
+    my @words = qw/a1 b2 c3 d4 e5 f6/;
 
-push(@words, $k, $opt);
+    # length for combinatorics, use only integer!
+    my $k = 4;
 
-print combinations(@words),"\n"}
+    print "---Combinations without repetition---\n\n";
+    print combinations_without_repetition(\@words, $k);
+    print "\n";
+    }
 
-{my @words = qw/a1 b2 c3/; #array for combinatorics
+    {
+    # array for combinatorics
+    my @words = qw/a1 b2 c3/;
 
-my $k = 3; #length for combinatorics, use only integer!
-my $opt = 2; #option: 2 - with repetition
+    # length for combinatorics, use only integer!
+    my $k = 3;
 
-push(@words, $k, $opt);
+    print "---Combinations with repetition---\n\n";
+    print combinations_with_repetition(\@words, $k);
+    }
 
-print combinations(@words),"\n"}
+    Output:
+    -------
 
-Result:
+    ---Combinations without repetition---
 
-(Combinations without repetition):
+    (1) a1 b2 c3 d4
+    (2) a1 b2 c3 e5
+    (3) a1 b2 c3 f6
+    (4) a1 b2 d4 e5
+    (5) a1 b2 d4 f6
+    (6) a1 b2 e5 f6
+    (7) a1 c3 d4 e5
+    (8) a1 c3 d4 f6
+    (9) a1 c3 e5 f6
+    (10) a1 d4 e5 f6
+    (11) b2 c3 d4 e5
+    (12) b2 c3 d4 f6
+    (13) b2 c3 e5 f6
+    (14) b2 d4 e5 f6
+    (15) c3 d4 e5 f6
 
-(1) a1 b2 c3 d4
-(2) a1 b2 c3 e5
-(3) a1 b2 c3 f6
-(4) a1 b2 d4 e5
-(5) a1 b2 d4 f6
-(6) a1 b2 e5 f6
-(7) a1 c3 d4 e5
-(8) a1 c3 d4 f6
-(9) a1 c3 e5 f6
-(10) a1 d4 e5 f6
-(11) b2 c3 d4 e5
-(12) b2 c3 d4 f6
-(13) b2 c3 e5 f6
-(14) b2 d4 e5 f6
-(15) c3 d4 e5 f6
+    ---Combinations with repetition---
 
-(Combinations with repetition):
-
-(1) a1 a1 a1
-(2) a1 a1 b2
-(3) a1 a1 c3
-(4) a1 b2 b2
-(5) a1 b2 c3
-(6) a1 c3 c3
-(7) b2 b2 b2
-(8) b2 b2 c3
-(9) b2 c3 c3
-(10) c3 c3 c3
+    (1) a1 a1 a1
+    (2) a1 a1 b2
+    (3) a1 a1 c3
+    (4) a1 b2 b2
+    (5) a1 b2 c3
+    (6) a1 c3 c3
+    (7) b2 b2 b2
+    (8) b2 b2 c3
+    (9) b2 c3 c3
+    (10) c3 c3 c3
 
 =head1 DESCRIPTION
 
-Math::Combination_out - Combinations without or with repetition
+Math::Combination_out - Combinations without/with repetition
+
+In this module was applied the approach for k-combinations without and with repetition 
+in lexicographic order, presented in the ANSI-C code by Siegfried Koepf at:
+
+http://www.aconnect.de/friends/editions/computer/combinatoricode_e.html
+
+=head1 EXPORT
+
+combinations_without_repetition
+combinations_with_repetition
+
+The result is a string, equal to the full output
 
 =head1 AUTHOR
 
@@ -185,21 +200,26 @@ Petar Kaleychev <petar.kaleychev@gmail.com>
 
 Report them to the author
 
+=head1 SEE ALSO
+
+L<Math::Combinatorics>,
+L<Algorithm::Combinatorics> 
+
 =head1 COPYRIGHT
 
 Copyright (c) 2011-2012 Petar Kaleychev
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut
